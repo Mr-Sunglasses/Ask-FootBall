36531a92fcc446d6441c399932250c8b9b1ed073
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-01-12 07:50:47 +0530

    :zap: Feat: improved cases

--- src/ask_football/main.py
+++ src/ask_football/main.py
@@ -7,7 +7,7 @@ def find_columns_and_filters(table_headers, question):
 
     column_patterns = {
         "Year": r"\b(years?|date|period)\b",
-        "Host": r"\b(hosts?|country|nation|where|location)\b",
+        "Host": r"\b(hosts?|country|nation|where|location|held)\b",
         "Venues_Cities": r"\b(venue|venues|city|cities|location|locations|place|places|where)\b",
         "Total_Attendance": r"\b(total attendance|attendees|people)\b",
         "Matches": r"\b(match|matches|game|games|finals)\b",
@@ -16,12 +16,19 @@ def find_columns_and_filters(table_headers, question):
         "Highest_Attendance_Venue": r"\b(venue|stadium|stadiums)\b",
         "Highest_Attendance_Games": r"\b(most watched game|finals)\b",
         # Ref: https://github.com/scrapinghub/dateparser/blob/master/dateparser/data/date_translation_data/en.py
+        # More Keywords we can add to expand the scope
+        # data ref; https://en.wikipedia.org/wiki/FIFA_World_Cup#Attendance
     }
 
-    for header in table_headers:
+    for header in ["Host", "Venues_Cities"]:
         pattern = column_patterns.get(header)
         if pattern and re.search(pattern, question, re.IGNORECASE):
             relevant_columns.append(header)
+
+    if not relevant_columns:
+        for header, pattern in column_patterns.items():
+            if re.search(pattern, question, re.IGNORECASE):
+                relevant_columns.append(header)
 
     year_match = re.search(r"\b\d{4}\b", question)
     year_range_match = re.search(r"\b(\d{4})-(\d{4})\b", question)
